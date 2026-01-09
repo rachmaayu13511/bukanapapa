94eb640975b98eb6d8d78b6531d2e130bf9121d5
rachmaayu13511 <rachmaayu13511@gmail.com>
2026-01-09 21:17:59 +0700

    Delete DesaKalibagor directory

--- DesaKalibagor/data/SPBU_10.js
+++
@@ -1,1 +0,0 @@
-var json_SPBU_10 = {"type":"FeatureCollection","name":"SPBU_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","Nama":"SPBU Pertamina Kalibagor"},"geometry":{"type":"Point","coordinates":[109.29800133374259,-7.486483073391496]}}]}
